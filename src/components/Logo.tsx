05c29695526a71c57c1a36c548f62aee64cee755
Justin Johnson <just4justin02@gmail.com>
2025-10-29 10:29:32 +0000

    this is the logo of the website https://ik.imagekit.io/ggelm1lwa/Dharma

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,22 +1,16 @@
-import { cn } from "@/lib/utils";
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
-export default function Logo(props: React.SVGProps<SVGSVGElement>) {
+export default function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn("h-8 w-8 text-primary", props.className)}
-      {...props}
-    >
-      <path d="M50 95C74.8528 95 95 74.8528 95 50C95 25.1472 74.8528 5 50 5C25.1472 5 5 25.1472 5 50C5 63.8071 16.1929 75 30 75" />
-      <path d="M42 33c0-8.8366 7.1634-16 16-16s16 7.1634 16 16" />
-      <path d="M83 26c-1.6667 4.6667-10 12-22 10" />
-      <path d="M38 58c0-9.9411 8.0589-18 18-18 8.0569 0 14.897 5.2866 17.156 12.5" />
-    </svg>
+    <div className={cn("relative h-8 w-8", className)} {...props}>
+      <Image
+        src="https://ik.imagekit.io/ggelm1lwa/DharmaGranth-Logo_1-removebg-preview.png?updatedAt=1761733737365"
+        alt="Dharmagranth TV Logo"
+        fill
+        sizes="32px"
+        className="object-contain"
+      />
+    </div>
   );
 }
