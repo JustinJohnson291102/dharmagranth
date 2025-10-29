01c918fe0fb2556c2edc57c2583d63541d12c850
Justin Johnson <just4justin02@gmail.com>
2025-10-29 10:33:49 +0000

    double the height of the navbar and that of the logo too

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,12 +3,12 @@ import { cn } from '@/lib/utils';
 
 export default function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn("relative h-8 w-8", className)} {...props}>
+    <div className={cn("relative h-16 w-16", className)} {...props}>
       <Image
         src="https://ik.imagekit.io/ggelm1lwa/DharmaGranth-Logo_1-removebg-preview.png?updatedAt=1761733737365"
         alt="Dharmagranth TV Logo"
         fill
-        sizes="32px"
+        sizes="64px"
         className="object-contain"
       />
     </div>
